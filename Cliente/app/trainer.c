20add723fc893679643722ad1a0f37b96113e1e0
lelv <imlelv@gmail.com>
2015-09-15 21:40:10 -0300

    terminal fachera

--- Cliente/app/trainer.c
+++ Cliente/app/trainer.c
@@ -5,25 +5,64 @@
 #include "../../commons/myTypes.h"
 #define forever while(1)
 
+void generatePokemons(POKEMON pokemones[6]);
+void printPokemons(POKEMON pokemones[6]);
+
 int main(){
 
-	srand(time(NULL));
 	POKEMON pokemones[6];
-	int i;
-	for(i = 0; i < 6; i++){
-		strcpy(pokemones[i].name, POKEMON_NAMES[rand()%150]);
-		pokemones[i].life = i * 10;
-	}
+	generatePokemons(pokemones);
+
+	printf("------------------- POKEMON CENTER -------------------\n\n");
+	printf("Your pokemons are: \n");
+	printPokemons(pokemones);
+
 
 	char buffer[256];
-	printf("hola\n");
 
 	forever{
-		printf("Qué desea hacer?\n");
+		printf("--> What do you want to do?\n$ ");
 		bzero(buffer, 256);
 		fgets(buffer, 255, stdin);
-		curar_pokemones(pokemones, 6);
+
+		if(strcmp(buffer,"heal\n") == 0){
+			curar_pokemones(pokemones, 6);
+		}else if (strcmp(buffer, "new pokemons\n") == 0)
+		{
+			generatePokemons(pokemones);
+			printf("Your new pokemons are\n");
+			printPokemons(pokemones);
+		}else if(strcmp(buffer, "my pokemons\n") == 0){
+			printPokemons(pokemones);
+		}else{
+			printf("Invalid command\n");
+		}
+		
 	
   }
+
+}
+
+void generatePokemons(POKEMON pokemones[6]){
+	srand(time(NULL));
+	int i;
+	int cant = rand()%3 + 4;
+	for(i = 0; i < cant; i++){
+		strcpy(pokemones[i].name, POKEMON_NAMES[rand()%150]);
+		pokemones[i].life = rand()%70;
+	}
+	for(i = cant; i < 6; i++){
+		pokemones[i].life = -1;
+	}
+
+}
+
+void printPokemons(POKEMON pokemones[6]){
+	int i;
+	int j = 1;
+	for (i= 0; i<6 && pokemones[i].life != -1; i++){
+		printf("   %d) %s\t--> Life: %d%% \n", j, pokemones[i].name, pokemones[i].life);
+		j++;
+	}
 
 }
--- Servidor/app/pokemonCenter.c
+++ Servidor/app/pokemonCenter.c
@@ -3,7 +3,7 @@
 
 int curar_pokemones(POKEMON *pokemones, int cant){
 	int i;
-	for(i = 0; i<cant; i++){
+	for(i = 0; i<cant && pokemones[i].life != -1; i++){
 		printf("Curando a: %s\n", pokemones[i].name );
 		usleep(10000* (100-pokemones[i].life));
 		pokemones[i].life = 100;
